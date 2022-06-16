6813ef88bbe69451cb3bcddcbb1557af8d27f225
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2022-06-16 12:08:01 +0200

    revise to make (a lot) faster

--- R/get_concept.R
+++ R/get_concept.R
@@ -2,7 +2,7 @@
 #'
 #' @param ... combination of column name and value to filter that column by. The
 #'   value to filter by can be provided as regular expression.
-#' @param exact [`logical(1)`][logical]\cr whether or not the value in
+#' @param regex [`logical(1)`][logical]\cr whether or not the value in
 #'   \code{...} shall be matched in full, or whether any partial match should be
 #'   returned.
 #' @param tree [`logical(1)`][logical]\cr whether or not to output the whole
@@ -19,7 +19,8 @@
 #' get_concept(label_en = "FODDER CROPS", path = ontoDir)
 #'
 #' # use regular expressions ...
-#' get_concept(label_en = "/*crops", exact = FALSE, path = ontoDir)
+#' get_concept(label_en = "/*crops", regex = TRUE, path = ontoDir)
+#' get_concept(label_en = "/*crops", broader = "_05", regex = TRUE, path = ontoDir)
 #'
 #' # get all concepts that are nested into another concept
 #' get_concept(label_en = "FODDER CROPS", tree = TRUE, path = ontoDir)
@@ -29,14 +30,14 @@
 #' @importFrom tibble as_tibble
 #' @importFrom readr read_rds
 #' @importFrom tidyselect everything
-#' @importFrom rlang quos eval_tidy
+#' @importFrom rlang exprs eval_tidy := sym
 #' @importFrom dplyr filter pull select
-#' @importFrom purrr map_lgl
+#' @importFrom purrr map map_dfc
 #' @importFrom stringr str_which str_sub
 #' @importFrom magrittr set_names
 #' @export
 
-get_concept <- function(..., exact = TRUE, tree = FALSE, missing = FALSE, #labels = FALSE,
+get_concept <- function(..., regex = FALSE, tree = FALSE, missing = FALSE,
                         path = NULL){
 
   if(!is.null(path)){
@@ -45,16 +46,19 @@ get_concept <- function(..., exact = TRUE, tree = FALSE, missing = FALSE, #label
     path <- getOption("onto_path")
   }
 
+  assertLogical(x = regex, len = 1, any.missing = FALSE)
+  assertLogical(x = tree, len = 1, any.missing = FALSE)
+  assertLogical(x = missing, len = 1, any.missing = FALSE)
+  if(regex & missing){
+    stop("you can only search for missing items with 'regex = FALSE'.")
+  }
+
   ontology <- read_rds(file = path)
-  onto <- left_join(ontology$attributes %>% filter(source %in% c("harmonised", "imported")),
+  onto <- left_join(ontology$attributes,
                     ontology$mappings %>% select(-label_en, -class), by = "code") %>%
     select(code, label_en, class, everything())
-  extConcepts <- ontology$attributes %>% filter(!source %in% c("harmonised", "imported"))
 
-  assertLogical(x = exact, len = 1, any.missing = FALSE)
-  assertLogical(x = tree, len = 1, any.missing = FALSE)
-
-  attrib <- quos(...)
+  attrib <- exprs(..., .named = TRUE)
   # return(attrib)
 
   if(length(attrib) == 0){
@@ -69,86 +73,122 @@ get_concept <- function(..., exact = TRUE, tree = FALSE, missing = FALSE, #label
     attrib <- attrib[sbst]
   }
 
-  temp <- data.frame(matrix(ncol = length(colnames(onto)), nrow = 0, data = NA_character_)) %>%
-    as_tibble() %>%
-    set_names(colnames(onto))
-  empty <- data.frame(matrix(ncol = length(colnames(onto)), nrow = 1, data = NA_character_)) %>%
-    as_tibble() %>%
-    set_names(colnames(onto))
+  # temp <- data.frame(matrix(ncol = length(colnames(onto)), nrow = 0, data = NA_character_)) %>%
+  #   as_tibble() %>%
+  #   set_names(colnames(onto))
+  # empty <- data.frame(matrix(ncol = length(colnames(onto)), nrow = 1, data = NA_character_)) %>%
+  #   as_tibble() %>%
+  #   set_names(colnames(onto))
 
-  for(i in seq_along(attrib)){
-    toSearch <- as.character(eval_tidy(attrib[[i]]))
-    missTemp <- NULL
+  codes <- map(.x = seq_along(attrib), .f = function(ix){
+    if(regex){
 
-    for(j in seq_along(toSearch)){
+      onto %>%
+        filter(str_detect(onto[[names(attrib)[ix]]], paste0(attrib[[ix]], collapse = "|"))) %>%
+        pull(code)
 
-      pos <- str_which(string = onto[[names(attrib)[i]]], pattern = toSearch[j])
-      posExt <- str_which(string = extConcepts[[names(attrib)[i]]], pattern = toSearch[j])
+    } else{
 
-      if(exact){
-        if(is.na(toSearch[j]) | toSearch[j] == ""){
-          newTab <- empty
-        } else {
-          if(length(pos) == 0 & length(posExt) == 0){
-            if(missing){
-              missTemp <- c(missTemp, toSearch[j])
-              next
-            } else{
-              stop(paste0("the concept '", toSearch[j], "' (", j, ") is not yet defined."))
-            }
-          } else {
-            if(length(pos) != 0){
-              newTab <- onto[onto[[names(attrib)[i]]] %in% toSearch[j],]
-            } else if(length(posExt) != 0){
-              extTab <- extConcepts$code[posExt]
+      onto %>%
+        filter(!!sym(names(attrib)[ix]) %in% attrib[[ix]]) %>%
+        pull(code)
 
-              posExt <- map_lgl(seq_along(onto$external), function(ix){
-                temp <- str_split(onto$external[ix], ", ")[[1]]
-                if(any(temp %in% extTab)){
-                  return(TRUE)
-                } else {
-                  return(FALSE)
-                }
-              })
-              newTab <- onto[posExt,]
-            }
-          }
-        }
-      } else {
-        newTab <- onto[pos,]
-      }
+    }
+  })
 
-      if(dim(newTab)[1] != 1 & exact){
-        warning(paste0("concept ", j, " (", toSearch[j],") has ", dim(newTab)[1], " entries"), call. = FALSE)
-      }
+  codes <- Reduce(intersect, codes)
 
-      if(j == 1){
-        temp <- newTab
-      } else {
-        temp <- temp %>%
-          bind_rows(newTab)
-      }
+  temp <- onto %>%
+    filter(code %in% codes)
+
+  # if missing = TRUE (regex must be FALSE) and thus we can join the input
+  # concepts with the temp output  to find which concepts are missing
+  if(missing){
+
+    temp <- map_dfc(.x = seq_along(attrib), .f = function(ix){
+
+      tibble(!!sym(names(attrib)[ix]) := attrib[[ix]])
+
+    }) %>%
+      expand.grid() %>%
+      as_tibble() %>%
+      left_join(temp, by = names(attrib)) %>%
+      filter(is.na(code))
+
+  } else {
+
+    if(tree){
+
+      topID <- temp %>%
+        pull(code) %>%
+        unique()
+
+      temp <- make_tree(onto, topID)
 
     }
 
   }
 
-  if(tree){
+  out <- temp %>%
+    select(code, broader, label_en, class, external, source)
 
-    topID <- temp %>%
-      pull(code) %>%
-      unique()
+  # for(i in seq_along(attrib)){
+  #   toSearch <- as.character(eval_tidy(attrib[[i]]))
+  #   outMiss <- NULL
+  #
+  #   for(j in seq_along(toSearch)){
+  #
+  #     pos <- str_which(string = onto[[names(attrib)[i]]], pattern = toSearch[j])
+  #     posExt <- str_which(string = extConcepts[[names(attrib)[i]]], pattern = toSearch[j])
+  #
+  #     if(regex){
+  #       if(is.na(toSearch[j]) | toSearch[j] == ""){
+  #         newTab <- empty
+  #       } else {
+  #         if(length(pos) == 0 & length(posExt) == 0){
+  #           if(missing){
+  #             outMiss <- c(outMiss, toSearch[j])
+  #             next
+  #           } else{
+  #             stop(paste0("the concept '", toSearch[j], "' (", j, ") is not yet defined."))
+  #           }
+  #         } else {
+  #           if(length(pos) != 0){
+  #             newTab <- onto[onto[[names(attrib)[i]]] %in% toSearch[j],]
+  #           } else if(length(posExt) != 0){
+  #             extTab <- extConcepts$code[posExt]
+  #
+  #             posExt <- map_lgl(seq_along(onto$external), function(ix){
+  #               temp <- str_split(onto$external[ix], ", ")[[1]]
+  #               if(any(temp %in% extTab)){
+  #                 return(TRUE)
+  #               } else {
+  #                 return(FALSE)
+  #               }
+  #             })
+  #             newTab <- onto[posExt,]
+  #           }
+  #         }
+  #       }
+  #     } else {
+  #       newTab <- onto[pos,]
+  #     }
+  #
+  #     if(dim(newTab)[1] != 1 & regex){
+  #       warning(paste0("concept ", j, " (", toSearch[j],") has ", dim(newTab)[1], " entries"), call. = FALSE)
+  #     }
+  #
+  #     if(j == 1){
+  #       temp <- newTab
+  #     } else {
+  #       temp <- temp %>%
+  #         bind_rows(newTab)
+  #     }
+  #
+  #   }
+  #
+  # }
 
-    temp <- make_tree(onto, topID)
+  return(out)
 
-  }
-
-  temp <- temp %>%
-    select(code, broader, label_en, class, external)
-
-  if(missing){
-    return(missTemp)
-  } else {
-    return(temp)
-  }
 }
