b989b5086f5da19bacdb34b85540b8cff491b547
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2022-06-21 11:21:41 +0200

    arrange this before building the tree

--- R/make_tree.R
+++ R/make_tree.R
@@ -3,12 +3,14 @@
 #' @param input [`tibble(1)`][tibble]\cr the concepts table of an ontology.
 #' @param top [`character(1)`][character]\cr the ID from which to derive the
 #'   tree.
-#' @importFrom dplyr filter pull
+#' @importFrom dplyr filter pull arrange
 
 make_tree <- function(input, top){
 
   fin <- NULL
   outIDs <- top
+  input <- input %>%
+    arrange(code)
   while(is.null(fin)){
     childID <- input %>%
       filter(broader %in% top) %>%
