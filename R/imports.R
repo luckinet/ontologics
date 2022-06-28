88609e8a76939368b99b58e928d21c0935eab649
Steffen Ehrmann <steffen.ehrmann@idiv.de>
2022-06-28 13:58:32 +0200

    revise pipe usage

--- R/imports.R
+++ R/imports.R
@@ -1,8 +1,15 @@
-#' re-export magrittr pipe operator
+#' Pipe operator
 #'
-#' @return No return value, simply re-exporting the pipe operator
-#' @importFrom magrittr %>%
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
 #' @name %>%
 #' @rdname pipe
+#' @keywords internal
 #' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+#' @param lhs A value or the magrittr placeholder.
+#' @param rhs A function call using the magrittr semantics.
+#' @return The result of calling `rhs(lhs)`.
 NULL
+
