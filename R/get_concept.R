c523bd7e4355753399897498cbf8cbef6549b813
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2022-07-04 13:49:25 +0200

    adapt the way to extract concepts

--- R/get_concept.R
+++ R/get_concept.R
@@ -1,6 +1,6 @@
 #' Get a concept in an ontology
 #'
-#' @param terms [`character(1)`][character]\cr
+#' @param x [`character(1)`][character]\cr
 #' @param ... combination of column name and value to filter that column by. The
 #'   value to filter by can be provided as regular expression.
 #' @param regex [`logical(1)`][logical]\cr whether or not the value in
@@ -17,18 +17,18 @@
 #' onto <- load_ontology(path = ontoDir)
 #'
 #' # exact matches from a loaded ontology ...
-#' get_concept(terms = "FODDER CROPS", ontology = onto)
+#' get_concept(x = data.frame(label_en = "FODDER CROPS"), ontology = onto)
 #'
 #' # ... or one stored on the harddisc
-#' get_concept(terms = "FODDER CROPS", ontology = ontoDir)
+#' get_concept(x = data.frame(label_en = "FODDER CROPS"), ontology = ontoDir)
 #'
 #' # use regular expressions ...
-#' get_concept(terms = "/*crops", regex = TRUE, ontology = onto)
+#' get_concept(x = data.frame(label_en = "/*crops"), regex = TRUE, ontology = onto)
 #'
-#' get_concept(terms = "/*crops", broader = ".05", regex = TRUE, ontology = onto)
+#' get_concept(x = data.frame(label_en = "/*crops"), broader = ".05", regex = TRUE, ontology = onto)
 #'
 #' # get all concepts that are nested into another concept
-#' get_concept(terms = "FODDER CROPS", tree = TRUE, ontology = onto)
+#' get_concept(x = data.frame(label_en = "FODDER CROPS"), tree = TRUE, ontology = onto)
 #' @return A table of a subset of the ontology according to the values in
 #'   \code{...}
 #' @importFrom checkmate assertFileExists assertLogical testChoice
@@ -37,17 +37,19 @@
 #' @importFrom tidyselect everything
 #' @importFrom tidyr separate_rows
 #' @importFrom rlang quos eval_tidy := sym as_name
-#' @importFrom dplyr filter pull select rename
+#' @importFrom dplyr filter pull select rename inner_join
 #' @importFrom purrr map map_dfc
 #' @importFrom stringr str_which str_sub
 #' @importFrom magrittr set_names
 #' @importFrom utils head
 #' @export
 
-get_concept <- function(terms = NULL, ..., regex = FALSE, tree = FALSE, missing = FALSE,
-                        ontology = NULL){
+get_concept <- function(x = NULL, ..., regex = FALSE, tree = FALSE,
+                        missing = FALSE, ontology = NULL){
 
-  assertCharacter(x = terms, null.ok = TRUE)
+  # documentation for x is missing
+
+  assertDataFrame(x = x, null.ok = FALSE)
   assertLogical(x = regex, len = 1, any.missing = FALSE)
   assertLogical(x = tree, len = 1, any.missing = FALSE)
   assertLogical(x = missing, len = 1, any.missing = FALSE)
@@ -65,9 +67,9 @@ get_concept <- function(terms = NULL, ..., regex = FALSE, tree = FALSE, missing 
   }
 
   onto <- ontology@concepts %>%
-    left_join(ontology@labels, by = "code") %>%
-    left_join(ontology@sources %>% select(sourceID, sourceName), by = "sourceID") %>%
-    left_join(ontology@mappings, by = "code")
+    left_join(ontology@labels, by = "id") %>%
+    left_join(ontology@sources %>% select(source_id, source_label), by = "source_id") %>%
+    left_join(ontology@mappings, by = "id")
 
   attrib <- quos(..., .named = TRUE)
   # return(attrib)
@@ -82,9 +84,9 @@ get_concept <- function(terms = NULL, ..., regex = FALSE, tree = FALSE, missing 
 
   if(regex){
 
-    if(!is.null(terms)){
+    if(!is.null(x)){
       toOut <- onto %>%
-        filter(str_detect(label_en, terms))
+        filter(str_detect(label_en, x$label_en))
     } else {
       toOut <- onto
     }
@@ -98,28 +100,34 @@ get_concept <- function(terms = NULL, ..., regex = FALSE, tree = FALSE, missing 
 
   } else {
 
-    extConcp <- tibble(label_en = terms, external = terms)
+    assertNames(x = names(x), subset.of = c("id", "has_broader", "source_id", "class", "label_en", "source_label", "external_id"))
 
-    tempOut <- onto %>%
-      filter(label_en %in% terms)
+    # extConcp <- tibble(label_en = x, external = x)
+    # extConcp <- tibble(label_en = x)
+
+    # tempOut <- onto %>%
+    #   filter(label_en %in% x)
+
+    tempOut <- x %>%
+      left_join(onto, by = colnames(x))
 
     toOut <- tempOut %>%
-      filter(!is.na(class)) %>%
-      select(-external) %>%
-      left_join(extConcp, by = "label_en") %>%
-      distinct()
+      filter(!is.na(class))# %>%
+      # select(-external) %>%
+      # left_join(extConcp, by = "label_en") %>%
+      # distinct()
     toMatch <- tempOut %>%
       filter(is.na(class)) %>%
       filter(!label_en %in% toOut$label_en)
 
     if(dim(toMatch)[1] != 0){
       toOut <- onto %>%
-        filter(str_detect(external, paste0(toMatch$code, collapse = "|"))) %>%
-        separate_rows(external, sep = ", ") %>%
-        rename(extCode = external) %>%
-        left_join(toMatch %>% select(extCode = code, external = label_en), by = "extCode") %>%
-        filter(!is.na(external)) %>%
-        select(-extCode) %>%
+        filter(str_detect(external_id, paste0(toMatch$id, collapse = "|"))) %>%
+        separate_rows(external_id, sep = ", ") %>%
+        rename(extid = external_id) %>%
+        left_join(toMatch %>% select(extid = id, external_id = label_en), by = "extid") %>%
+        filter(!is.na(external_id)) %>%
+        select(-extid) %>%
         bind_rows(toOut, .)
     }
 
@@ -130,37 +138,33 @@ get_concept <- function(terms = NULL, ..., regex = FALSE, tree = FALSE, missing 
 
     }
 
-    toOut <- extConcp %>%
-      select(external) %>%
-      left_join(toOut, by = "external")
-
   }
 
   if(missing){
 
     temp <- toOut %>%
-      filter(is.na(code))
+      filter(is.na(id))
 
   } else {
 
     if(tree){
 
       topID <- toOut %>%
-        filter(sourceName %in% c("harmonised", "imported")) %>%
-        pull(code) %>%
+        filter(source_label %in% c("harmonised", "imported")) %>%
+        pull(id) %>%
         unique()
 
       temp <- make_tree(onto, topID)
 
     } else {
       temp <- toOut %>%
-        filter(!is.na(code))
+        filter(!is.na(id))
     }
 
   }
 
   out <- temp %>%
-    select(code, broader, label_en, class, external, sourceName)
+    select(id, has_broader, label_en, class, external_id, source_label)
 
   return(out)
 
