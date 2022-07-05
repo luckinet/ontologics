46bd2db4e2a9600560ed9f36c60d913866383b4c
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2022-07-05 11:10:39 +0200

    define new global functions

--- R/zzz.R
+++ R/zzz.R
@@ -2,5 +2,8 @@ globalVariables(c("id", "label_en", "new_id", "has_broader", "external",
                   "extCode", ".", "source_id", "source_label", "description",
                   "homepage", "notes", "level", "undef", "topID", "top2D",
                   "nestedID", "nextID", "newClass", "extid", "broader",
-                  "class_label", "external_id"
+                  "class_label", "external_id", "has_narrower_match",
+                  "has_close_match", "has_exact_match", "has_broader_match",
+                  "new_exact_match", "new_narrower_match", "new_close_match",
+                  "new_broader_match", "avail", "has_source", "label", "newid"
                   ))
