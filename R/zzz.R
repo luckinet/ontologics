31b2588cae24914e5736e5fb4c2a946028414ecd
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2022-07-20 02:36:34 +0200

    new global variables

--- R/zzz.R
+++ R/zzz.R
@@ -6,5 +6,5 @@ globalVariables(c("id", "label_en", "new_id", "has_broader", "external",
                   "has_close_match", "has_exact_match", "has_broader_match",
                   "new_exact_match", "new_narrower_match", "new_close_match",
                   "new_broader_match", "avail", "has_source", "label", "newid",
-                  "external_label"
+                  "external_label", "harmLab", "sort_in", "verbose", "new_label"
                   ))
