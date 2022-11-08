0221ae2a39f09a8648df113730176186c6dfd4a1
Steffen Ehrmann <steffen.ehrmann@posteo.de>
2022-11-09 00:38:54 +0100

    also store "has_broader" relation in external table and make use of it when mapping

--- R/new_mapping.R
+++ R/new_mapping.R
@@ -195,11 +195,11 @@ new_mapping <- function(new = NULL, target, source = NULL, description = NULL,
   }
 
   extMps <- temp %>%
-    distinct(new, description, has_source) %>%
+    distinct(new, description, has_broader, has_source) %>%
     filter(new != "") %>%
     filter(!(new %in% theTable$external$label & has_source %in% theTable$external$has_source)) %>%
     mutate(newid = paste0(source, "_", row_number() + prevID)) %>%
-    select(id = newid, label = new, description, has_source)
+    select(id = newid, label = new, has_broader, description, has_source)
 
   if(!all(is.na(description$desc)) & !dim(extMps)[1] == 0){
     extMps <- left_join(extMps, description, by = "label") %>%
@@ -215,7 +215,8 @@ new_mapping <- function(new = NULL, target, source = NULL, description = NULL,
 
     toOut <- temp %>%
       select(-description) %>%
-      left_join(theTable$external %>% filter(has_source == srcID) %>% select(new = label, newid = id), by = "new") %>%
+      left_join(theTable$external %>% filter(has_source == srcID) %>% select(new = label, newid = id, has_broader),
+                by = c("new", "has_broader")) %>%
       filter(!is.na(newid)) %>%
       mutate(newid = if_else(!is.na(newid), paste0(newid, ".", certainty), NA_character_),
              match = paste0("has_", match, "_match")) %>%
